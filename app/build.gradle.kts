feab07af357b9af5ed37206b823785d7165bc6fb
kirillS1 <krya_kirya@mail.ru>
2025-12-16 17:58:04 +0700

    Ver 0.3.12a - quickfix for the inputs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 1
-        versionName = "0.3.4b"
+        versionName = "0.3.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
